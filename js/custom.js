98013c7da9dea9911bfa1812f738d0675e2aeefc
Blue <564618583@qq.com>
2018-09-16 15:44:12 +0800

    new

--- js/custom.js
+++ js/custom.js
@@ -61,7 +61,7 @@ $(window).load(function(){
 
 // jQuery to collapse the navbar on scroll //
 $(window).scroll(function() {
-    if ($(".navbar").offset().top > 50) {
+    if ($(".navbar").offset().top > 759) {
         $(".navbar-fixed-top").addClass("top-nav-collapse");
     } else {
         $(".navbar-fixed-top").removeClass("top-nav-collapse");
@@ -106,73 +106,7 @@ $(function(){
         $(this).find("span").toggleClass("glyphicon-chevron-up");
     });
 });
-// $('#myModal').modal('show')
-// $('#exampleModal').on('show.bs.modal', function (event) {
-//     var button = $(event.relatedTarget) // Button that triggered the modal
-//     var recipient = button.data('whatever') // Extract info from data-* attributes
-//     // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-//     // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-//     var modal = $(this)
-//     modal.find('.modal-title').text('New message to ' + recipient)
-//     modal.find('.modal-body input').val(recipient)
-// })
 
 
-  // HOME BACKGROUND SLIDESHOW
-  // $(function(){
-  //   jQuery(document).ready(function() {
-  //   $('#home').backstretch([
-  //      // "images/home-bg-slideshow1.jpg",
-  //      // "images/home-bg-slideshow2.jpg",
-  //      //  "images/portfolio-img1.jpg"
-  //       ],  {duration: 2000, fade: 750});
-  //   });
-  // })
 
-
-//     $(function(){
-//         jQuery(document).ready(function() {
-//             $('#home2').backstretch([
-//                 // "images/home-bg-slideshow1.jpg",
-//                 "images/home-bg-slideshow2.jpg",
-//             ],  {duration: 2000, fade: 750});
-//         });
-//     })
-//
-//     $(function(){
-//         jQuery(document).ready(function() {
-//             $('#home3').backstretch([
-//                 "images/che1.jpg",
-//                 // "images/home-bg-slideshow2.jpg",
-//             ],  {duration: 2000, fade: 750});
-//         });
-//     })
-//
-//     $(function(){
-//         jQuery(document).ready(function() {
-//             $('#home4').backstretch([
-//                 "images/che2.jpg",
-//                 // "images/home-bg-slideshow2.jpg",
-//             ],  {duration: 2000, fade: 750});
-//         });
-//     })
-//
-//     $(function(){
-//         jQuery(document).ready(function() {
-//             $('#home5').backstretch([
-//                 "images/che3.jpg",
-//                 // "images/home-bg-slideshow2.jpg",
-//             ],  {duration: 2000, fade: 750 });
-//         });
-//     })
-//
-//     $(function(){
-//         jQuery(document).ready(function() {
-//             $('#home6').backstretch([
-//                 "images/che4.jpg",
-//                 // "images/home-bg-slideshow2.jpg",
-//             ],  {duration: 2000, fade: 750});
-//         });
-//     })
-// });
 
