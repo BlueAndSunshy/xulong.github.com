621c5931e8021ac32a08e8437c528e421dd91db2
Blue <564618583@qq.com>
2018-10-19 17:52:35 +0800

    2018-10-19

--- js/custom.js
+++ js/custom.js
@@ -2,53 +2,53 @@
 // ISOTOPE FILTER
 jQuery(document).ready(function($){
 
-  if ( $('.iso-box-wrapper').length > 0 ) { 
+    if ( $('.iso-box-wrapper').length > 0 ) {
 
-      var $container  = $('.iso-box-wrapper'), 
-        $imgs     = $('.iso-box img');
+        var $container  = $('.iso-box-wrapper'),
+            $imgs     = $('.iso-box img');
 
-      $container.imagesLoaded(function () {
+        $container.imagesLoaded(function () {
 
-        $container.isotope({
-        layoutMode: 'fitRows',
-        itemSelector: '.iso-box'
+            $container.isotope({
+                layoutMode: 'fitRows',
+                itemSelector: '.iso-box'
+            });
+
+            $imgs.load(function(){
+                $container.isotope('reLayout');
+            })
+
         });
 
-        $imgs.load(function(){
-          $container.isotope('reLayout');
-        })
+        //filter items on button click
 
-      });
+        $('.filter-wrapper li a').click(function(){
 
-      //filter items on button click
+            var $this = $(this), filterValue = $this.attr('data-filter');
 
-      $('.filter-wrapper li a').click(function(){
+            $container.isotope({
+                filter: filterValue,
+                animationOptions: {
+                    duration: 750,
+                    easing: 'linear',
+                    queue: false
+                }
+            });
 
-          var $this = $(this), filterValue = $this.attr('data-filter');
+            // don't proceed if already selected
 
-      $container.isotope({ 
-        filter: filterValue,
-        animationOptions: { 
-            duration: 750, 
-            easing: 'linear', 
-            queue: false
-        }                
-      });             
+            if ( $this.hasClass('selected') ) {
+                return false;
+            }
 
-      // don't proceed if already selected 
+            var filter_wrapper = $this.closest('.filter-wrapper');
+            filter_wrapper.find('.selected').removeClass('selected');
+            $this.addClass('selected');
 
-      if ( $this.hasClass('selected') ) { 
-        return false; 
-      }
+            return false;
+        });
 
-      var filter_wrapper = $this.closest('.filter-wrapper');
-      filter_wrapper.find('.selected').removeClass('selected');
-      $this.addClass('selected');
-
-        return false;
-      }); 
-
-  }
+    }
 
 });
 
